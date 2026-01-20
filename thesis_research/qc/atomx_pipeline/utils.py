feb301d6643088690543b6b2b7b28389b87b419d
chen_levy_arviv <chenrv1@gmail.com>
2026-01-20 23:38:53 +0200

    L321 slices

--- thesis_research/qc/atomx_pipeline/utils.py
+++ thesis_research/qc/atomx_pipeline/utils.py
@@ -1,2 +1,11 @@
+from anndata import AnnData
+
+
 def q(x, percentile):
     return x.quantile(percentile)
+
+
+def subset_adata_by_fovs(adata: AnnData, fovs: list[str]) -> AnnData:
+    fovs = set(map(str, fovs))
+    mask = adata.obs["fov"].astype(str).isin(fovs).to_numpy()
+    return adata[mask].copy()
